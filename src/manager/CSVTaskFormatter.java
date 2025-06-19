e0d8410ce374edb888bf1743b42a22d49d4fa537
Oleg Digital <boss@agerasiadigital.ru>
2025-06-20 00:06:15 +0300

    Изменил точность времени

--- src/manager/CSVTaskFormatter.java
+++ src/manager/CSVTaskFormatter.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class CSVTaskFormatter {
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     public static String toString(Task task) {
         String epicId = "";
